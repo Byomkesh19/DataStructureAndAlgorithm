f2cad59d813c4a77d91004e64b63f50f0529dd27
Byomkesh Choudhary <118907334+Byomkesh19@users.noreply.github.com>
2023-11-29 16:16:54 +0530

    Added leetcode solution of Number Of 1's Bits

---
+++ DataStructureAndAlgorithms/src/com/deewana/dsa/problems/leetcode/NumberOf1Bits.java
@@ -0,0 +1,9 @@
+package com.deewana.dsa.problems.leetcode;
+
+public class NumberOf1Bits {
+	public class Solution {
+	    public int hammingWeight(int n) {
+	        return Integer.bitCount(n);
+	    }
+	}
+}
