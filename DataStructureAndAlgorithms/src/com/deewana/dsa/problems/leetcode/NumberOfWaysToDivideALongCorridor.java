d13b04c65ae5ade6e8cde2b3a96dc6e26dd5dc0c
Byomkesh Choudhary <118907334+Byomkesh19@users.noreply.github.com>
2023-11-29 16:18:40 +0530

    Update NumberOfWaysToDivideALongCorridor.java(Faster Version)

--- DataStructureAndAlgorithms/src/com/deewana/dsa/problems/leetcode/NumberOfWaysToDivideALongCorridor.java
+++ DataStructureAndAlgorithms/src/com/deewana/dsa/problems/leetcode/NumberOfWaysToDivideALongCorridor.java
@@ -7,36 +7,27 @@ public class NumberOfWaysToDivideALongCorridor {
 	class Solution {
 	    public int numberOfWays(String corridor) {
 	        int scnt=0;
-	        List<Integer> ulist=new ArrayList<>();
-	        List<Integer> vlist=new ArrayList<>();
 	        int currCnt=0;
+	        int lastV=corridor.indexOf('S')-1;
 	        int corridorLength=corridor.length();
+	        int ans=1;
 	        for(int i=0;i<corridorLength;i++){
 	            char ch=corridor.charAt(i);
 	            if(ch=='S'){
 	                scnt++;
 	                if(currCnt==0){
-	                    ulist.add(i);
+	                    int cnt=i-lastV;
+	                    ans=(int)(((long)((long)ans*(long)cnt))%1000000007);
 	                    currCnt++;
 	                }
 	                else if(currCnt==1){
-	                    vlist.add(i);
+	                    lastV=i;
 	                    currCnt=0;
 	                }
 	            }
 	        }
 	        if(scnt%2==1 || scnt==0){
 	            return 0;
-	        }
-	        int ans=1;
-	        int n=ulist.size();
-	        int lastV=ulist.get(0)-1;
-	        for(int i=0;i<n;i++){
-	            int u=ulist.get(i);
-	            int v=vlist.get(i);
-	            int cnt=u-lastV;
-	            ans=(int)(((long)((long)ans*(long)cnt))%1000000007);
-	            lastV=v;
 	        }
 	        return ans;
 	    }
